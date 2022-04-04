5712f6feb26dca66e474924ec487ee18c5388661
KaroolNiemiec <karool.niemiec@gmail.com>
2022-04-04 21:46:44 +0200

    groundwork

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { CssBaseline, Grid } from "@mui/material"
+import Header from "./components/Header/Header";
+import Map from "./components/Map/Map";
+import List from "./components/List/List";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item sx={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item sx={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+export default App;
